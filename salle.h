bafe12e5aee2f6f090c75a38ec939dcff73a388b
Lou-AnneVAU <louanne.vlt52@gmail.com>
2025-01-14 15:32:59 +0100

    Add files via upload

---
+++ salle.h
@@ -0,0 +1,20 @@
+#ifndef SALLE_H
+#define SALLE_H
+#include <iostream>
+#include <string>
+class Salle
+{
+private:
+    int numero;
+    std::string typeSalle;
+
+public:
+    Salle();
+    Salle(int numero,std::string typeSalle);
+    int getNumero();
+    std::string getTypeSalle();
+    void deleteSalle();
+    void affiche();
+};
+
+#endif // SALLE_H
