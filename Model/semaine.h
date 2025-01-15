e02b26ba3a455f9d6af8a98ecbbe2c38a40d0bd8
Mae Mumi <maeva.ramamonjyramboa@reseau.eseo.fr>
2025-01-15 10:42:33 +0100

    Update semaine.h

--- Model/semaine.h
+++ Model/semaine.h
@@ -1,14 +1,14 @@
 #ifndef SEMAINE_H
 #define SEMAINE_H
 
-#include <vector>      // Pour std::vector
+#include <vector>      
 #include "Creneau.h"
-#include "main.h"// Inclusion de la classe Creneau
+#include "main.h"
 
 class Semaine {
 private:
-    uint32 numero;                      // Numéro de la semaine
-    std::vector<Creneau> listeCreneau;    // Liste dynamique de créneaux
+    uint32 numero;                   
+    std::vector<Creneau> listeCreneau;   
 
 public:
     Semaine();
@@ -17,8 +17,8 @@ public:
     uint32 getNumero() const;
     const std::vector<Creneau>& getListeCreneau() const;
 
-    void ajouterCreneau(const Creneau& creneau);  // Ajouter un créneau à la liste
-    void supprimerCreneau(int index);            // Supprimer un créneau par index
+    void ajouterCreneau(const Creneau& creneau);  
+    void supprimerCreneau(int index);        
 };
 
 #endif // SEMAINE_H
