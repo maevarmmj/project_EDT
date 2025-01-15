962eb8495c1926075217a67088c24790a629bb52
Mae Mumi <maeva.ramamonjyramboa@reseau.eseo.fr>
2025-01-15 08:17:57 +0100

    Update enseignant.h
    obvious comment removed :))

--- Model/enseignant.h
+++ Model/enseignant.h
@@ -6,24 +6,19 @@
 
 class Enseignant {
 private:
-    std::string nom;       // Nom de l'enseignant
-    std::string prenom;    // Prénom de l'enseignant
+    std::string nom;    
+    std::string prenom;   
 
 public:
-    // Constructeur par défaut
     Enseignant();
 
-    // Constructeur avec paramètres
     Enseignant(const std::string& nom, const std::string& prenom);
 
-    // Accesseurs en lecture
     std::string getNom() const;
     std::string getPrenom() const;
 
-    // Méthode affiche
     void affiche() const;
 
-    // Destructeur
     ~Enseignant();
 };
 
